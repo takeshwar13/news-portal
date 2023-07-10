21591163eab452025a616c863cdd186c42714888
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-07-10 20:07:41 +0530

    Added Some Styling in FetchNews Component

--- src/Component/FetchNews.js
+++ src/Component/FetchNews.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const FetchNews = () => {
   return (
-    <div>
-      <button className="btn btn-primary"> FetchNews</button>
+    <div className="container">
+      <div className="row">
+        <div className="clo-4">
+          <button className="btn btn-primary"> Fetch News</button>
+        </div>
+      </div>
     </div>
   );
 };
