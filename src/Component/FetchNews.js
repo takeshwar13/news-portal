f6291786d3f4ae12b1dd0cc02debec4217278177
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-07-11 00:04:14 +0530

    used Axios's get and response method to send https request and collect response respectively

--- src/Component/FetchNews.js
+++ src/Component/FetchNews.js
@@ -1,11 +1,24 @@
+import axios from "axios";
 import React from "react";
 
 const FetchNews = () => {
+  const FetchNewsHandler = () => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=36b883ab18f049ae8f832437f0f744ae"
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
   return (
     <div className="container">
       <div className="row">
         <div className="clo-4">
-          <button className="btn btn-primary"> Fetch News</button>
+          <button className="btn btn-primary" onClick={FetchNewsHandler}>
+            {" "}
+            Fetch News
+          </button>
         </div>
       </div>
     </div>
