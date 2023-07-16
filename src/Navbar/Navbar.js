8327d22ce222cdf1fad0e2030c94f578bfbf979d
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-07-16 21:19:28 +0530

    Added more elements/options in Navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,41 +3,52 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Home
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Sports <span className="sr-only"></span>
+              <a className="nav-link text-white" href="#" aria-current="page">
+                Home
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link text-white" href="#" aria-current="page">
+                General
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link text-white" href="#" aria-current="page">
+                Buisness
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link text-white" href="#" aria-current="page">
+                Sports
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link text-white" href="#" aria-current="page">
+                Entertenment
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#" aria-current="page">
                 Bollywood
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Buisness
+              <a className="nav-link text-white" href="#" aria-current="page">
+                Cricket
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Cricket
+              <a className="nav-link text-white" href="#" aria-current="page">
+                Helth
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#" aria-current="page">
+                Technology
               </a>
             </li>
           </ul>
