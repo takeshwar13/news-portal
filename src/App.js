08045efde9eba71f0a8fe9d7dd8b81ed9b28e122
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-07-17 20:23:45 +0530

    Set path for home and navbar components using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import FetchNews from "./Component/FetchNews";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <FetchNews />
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
