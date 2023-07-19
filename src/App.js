a5e872cfbc55cf127c020ce9a280fd2016760b39
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-07-19 23:21:30 +0530

    set Route/Path of General Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import Home from "./Pages/Home";
+import General from "./Pages/General";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navbar />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/general" element={<General general="General News " />} />
       </Routes>
     </div>
   );
