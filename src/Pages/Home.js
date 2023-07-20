61f0d3b506b6ae1cc9a38782b305b35435ac98cf
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-07-20 15:46:40 +0530

    Imported HomeDisplay Component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import FetchNews from "../Component/FetchNews";
+import HomeDisplay from "../Component/HomeDisplay";
 
 const Home = () => {
   return (
     <>
       <div>
+        <HomeDisplay />
         <FetchNews />
       </div>
     </>
