d4218504b80ab3476b8384fe9f0ea64b9edc3ea8
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-07-24 23:16:54 +0530

    Used useEffet to get props and change data

--- src/Component/FetchNews.js
+++ src/Component/FetchNews.js
@@ -1,38 +1,45 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useEffect } from "react";
 
-const FetchNews = () => {
+const FetchNews = (props) => {
   const [news, setNews] = useState([]);
-  const FetchNewsHandler = () => {
-    axios
-      .get(
-        "https://newsapi.org/v2/top-headlines?country=in&apiKey=36b883ab18f049ae8f832437f0f744ae"
-      )
-      .then((response) => {
-        console.log(response);
-        setNews(response.data.articles);
-      });
-  };
+
+  useEffect(() => {
+    const FetchNewsHandler = async () => {
+      await axios
+        .get(
+          props.cat
+            ? `https://newsapi.org/v2/top-headlines?country=in&category=${props.cat}&apiKey=36b883ab18f049ae8f832437f0f744ae`
+            : "https://newsapi.org/v2/top-headlines?country=in&apiKey=36b883ab18f049ae8f832437f0f744ae"
+        )
+        .then((response) => {
+          setNews(response.data.articles);
+        });
+    };
+
+    FetchNewsHandler();
+  }, [props.cat]);
   return (
     <>
       <div className="container my-3">
         <div className="row">
           <div className="clo-4">
-            <div className="headlines" onClick={FetchNewsHandler()}>
-              <h3>Top Headlines</h3>
-            </div>
+            <h4 className="headline d-flex justify-content-center aglign-items-center">
+              <u>TOP HEADLINES</u>
+            </h4>
           </div>
         </div>
       </div>
-      <div className="container my-3">
+      <div className="container my-2">
         <div className="row">
           {news.map((value, index) => {
             return (
               <div
-                className="col-4"
+                className="col-4 d-flex justify-content-center aglign-items-center"
                 style={{ boxShadow: "2px 2px 10px gray", borderRadius: "10px" }}
               >
-                <div class="card my-3 " style={{ width: "18rem" }}>
+                <div class="card my-3" style={{ width: "18rem" }}>
                   <img src={value.urlToImage} class="card-img-top" alt="..." />
                   <div class="card-body">
                     <div className="key">{value.index}</div>
